20a62d2a0a2669c86c556c196a0c3db81319615e
AlJawharhALOtaibi <ALJawharhALOtaibi@gmail.com>
2024-02-01 17:42:22 +0300

    New file

---
+++ Wntil/Github test.swift
@@ -0,0 +1,18 @@
+//
+//  Github test.swift
+//  Wntil
+//
+//  Created by AlJawharh AlOtaibi on 20/07/1445 AH.
+//
+
+import SwiftUI
+
+struct Github_test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Github_test()
+}
